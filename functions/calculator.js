6f19e6e127da2e790f30a70d04c3ebfb8d5e223a
Yul <yul_klj@hotmail.com>
2021-02-16 17:00:17 +0800

    #9 adds estimate day to get payout

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const approxEarn = process.env.NANOPOOL_BASE_URL + 'approximated_earnings/';
 const totalReportedHashRate = process.env.NANOPOOL_BASE_URL + 'reportedhashrate/' + process.env.WALLET_ADDRESS;
+const balance = process.env.NANOPOOL_BASE_URL + 'balance_hashrate/' + process.env.WALLET_ADDRESS;
 
 module.exports = function(bot, notifyChatId, chatId) {
   var convertTZ = function(date, tzString) {
@@ -17,6 +18,7 @@ module.exports = function(bot, notifyChatId, chatId) {
   request(totalReportedHashRate, function (error, response, body) {
     let totalHash = 0;
     let res;
+    let dailyCoinEarn = 0;
     if (!error && response.statusCode == 200) {
       res = JSON.parse(body);
       totalHash = res['data'];
@@ -24,11 +26,21 @@ module.exports = function(bot, notifyChatId, chatId) {
         let line = 'Approx Calculator:-\n';
         if (!error && response.statusCode == 200) {
           res = JSON.parse(body);
+          dailyCoinEarn = res['data']['day']['coins'];
           line += `Day: ${parseFloat(res['data']['day']['coins']).toFixed(6)}\n`;
           line += `Week: ${parseFloat(res['data']['week']['coins']).toFixed(6)}\n`;
           line += `Month: ${parseFloat(res['data']['month']['coins']).toFixed(6)}\n`;
           line += `Each ETH Price USD: ${res['data']['prices']['price_usd']}\n`;
-          bot.sendMessage(chatId, line);
+
+          request(balance, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              const res = JSON.parse(body);
+              const estimateDay = (0.1 - res['data']['balance']) / dailyCoinEarn;
+              line += `\nEstimate reaches 0.1 in:\n${parseFloat(estimateDay).toFixed(2)} Day(s) \n`;
+
+              bot.sendMessage(chatId, line);
+            }
+          });
         }
       });
     }
