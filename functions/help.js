7815bfad8951e4863d3297b81585bd516f172007
Yul <yul_klj@hotmail.com>
2021-02-18 15:00:42 +0800

    #10 help menu presists

--- functions/help.js
+++ functions/help.js
@@ -2,11 +2,12 @@ const functionsCaseKeys = require('../functions_case.js').keys;
 
 module.exports = function(bot, notifyChatId, chatId) {
   const structure = functionsCaseKeys().map(x => [x]);
-  bot.sendMessage(chatId, 'Choices Below', {
+  bot.sendMessage(chatId, 'Help choices', {
     'reply_markup': {
         'keyboard': structure,
+        hide_keyboard: true,
         resize_keyboard: true,
-        one_time_keyboard: true,
+        one_time_keyboard: false,
         force_reply: true,
     }
   });
