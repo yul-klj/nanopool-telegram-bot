2adb28ce28f1cf81d32bcbe7ba14d82fe9f22334
Yul <yul_klj@hotmail.com>
2021-02-18 14:45:07 +0800

    #10 refactor report worker(hashrate)function to async await

--- functions/report-worker.js
+++ functions/report-worker.js
@@ -1,29 +1,26 @@
+const util = require('util');
 const request = require('request');
 const reportedHashRates = process.env.NANOPOOL_BASE_URL + 'reportedhashrates/' + process.env.WALLET_ADDRESS;
 const workers = process.env.NANOPOOL_BASE_URL + 'workers/' + process.env.WALLET_ADDRESS;
 
-module.exports = function(bot, notifyChatId, chatId) {
-  request(workers, function (error, response, body) {
-    let line = '';
-    if (!error && response.statusCode == 200) {
-      const res = JSON.parse(body);
-      for (i in res['data']) {
-        line += `Workers Current:-\n${res['data'][i]['id']}: [${res['data'][i]['hashrate']}]\n`;
-      }
-      bot.sendMessage(chatId, line);
+module.exports = async function(bot, notifyChatId, chatId) {
+  const requestPromise = util.promisify(request);
+  const workerReq = await requestPromise(workers);
+  const reportHashRateReq = await requestPromise(reportedHashRates);
+
+  let line = 'Something wrong with the api';
+  if (workerReq.statusCode == 200 && reportHashRateReq.statusCode == 200) {
+    line = '';
+    const workerRes = JSON.parse(workerReq.body);
+    for (i in workerRes['data']) {
+      line += `Current Hash:-\n${workerRes['data'][i]['id']}: [${workerRes['data'][i]['hashrate']}]\n`;
     }
-  });
 
-
-  request(reportedHashRates, function (error, response, body) {
-    let line = '';
-    if (!error && response.statusCode == 200) {
-      const res = JSON.parse(body);
-      for (i in res['data']) {
-        line += `Workers Reported:-\n${res['data'][i]['worker']}: [${res['data'][i]['hashrate']}]\n`;
-      }
+    const reportHashRateRes = JSON.parse(reportHashRateReq.body);
+    for (i in reportHashRateRes['data']) {
+      line += `Reported Hash:-\n${reportHashRateRes['data'][i]['worker']}: [${reportHashRateRes['data'][i]['hashrate']}]\n`;
     }
-    bot.sendMessage(chatId, line);
-  });
+  }
 
+  bot.sendMessage(chatId, line);
 };
