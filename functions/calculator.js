3c0db4676731fabbfb430769900a87c669468e8c
Yul <yul_klj@hotmail.com>
2021-02-25 18:54:40 +0800

    Remove unuse code

--- functions/calculator.js
+++ functions/calculator.js
@@ -6,19 +6,6 @@ const balance = process.env.NANOPOOL_BASE_URL + 'balance_hashrate/' + process.en
 const payoutSetting = process.env.NANOPOOL_BASE_URL + 'usersettings/' + process.env.WALLET_ADDRESS;
 
 module.exports = async function(bot, notifyChatId, chatId) {
-  var convertTZ = function(date, tzString) {
-    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-GB", {timeZone: tzString}));
-  }
-  var today = convertTZ(new Date(), "Asia/KUALA_LUMPUR").toLocaleDateString('en-GB', {
-    day:   'numeric',
-    month: 'short',
-    year:  'numeric',
-    hour:   '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-  });
-
-  let totalHash = 0;
   let dailyCoinEarn = 0;
 
   const requestPromise = util.promisify(request);
